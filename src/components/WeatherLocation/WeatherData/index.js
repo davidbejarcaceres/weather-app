49196188ae0d8afe9434e289acf7efef35bb1362
David <davidbejar2009@gmail.com>
2020-01-24 00:30:48 +0100

    added animations, api fetching and async

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,47 +1,44 @@
-import React, { Component } from 'react';
-import WeatherExtraInfo from './WeatherExtraInfo';
-import WeatherTemperature from './WeatherTemperature'
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import WeatherExtraInfo from "./WeatherExtraInfo";
+import WeatherTemperature from "./WeatherTemperature";
+import PropTypes from "prop-types";
+import { useState } from "react";
+
 import {
-    CLOUD,
-    RAIN,
-    SUN,
-    SNOW,
-    WINDY,
-    CLOUDY,
-    FOG
-} from '../../../consants/weather'
+  CLOUD,
+  RAIN,
+  SUN,
+  SNOW,
+  WINDY,
+  CLOUDY,
+  FOG
+} from "../../../consants/weather";
 
-import "./styles.css"
+import "./styles.css";
 
-class WeatherData extends Component {
-
-    constructor(props) {
-        super(props);
-        this.data = this.props.data;
-    }
-
-    render() {
-        console.log(this.data.weatherState);
-
-        return (
-            <div className="weatherDataCont">
-                <WeatherTemperature temperature={this.data.temperature} weatherState={this.data.weatherState}></WeatherTemperature>
-                <WeatherExtraInfo humidity={this.data.humidity} wind={this.data.wind}></WeatherExtraInfo>                
-            </div>
-        );
-    }
-}
+const WeatherData = props => {
+  const data = props.data;
+  return (
+    <div className="weatherDataCont">
+      <WeatherTemperature
+        temperature={data.temperature}
+        weatherState={data.weatherState}
+      ></WeatherTemperature>
+      <WeatherExtraInfo
+        humidity={data.humidity}
+        wind={data.wind}
+      ></WeatherExtraInfo>
+    </div>
+  );
+};
 
 export default WeatherData;
 
-
 WeatherData.propTypes = {
-    data: PropTypes.shape({
-        temperature: PropTypes.number.isRequired,
-        weatherState: PropTypes.string.isRequired,
-        humidity: PropTypes.number.isRequired,
-        wind: PropTypes.string.isRequired
-    })
-
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired
+  })
 };
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,43 +1,87 @@
 import React from "react";
 import Location from "./Location";
-import WeatherData from "./WeatherData"
-import PropTypes from 'prop-types';
+import WeatherData from "./WeatherData";
+import PropTypes from "prop-types";
 import { SUN, WINDY } from "../../consants/weather";
 import { useState } from "react";
 
-const data = {
-     temperature: 5,
-     weatherState: SUN,
-     humidity: 10,
-     wind: "10 m/s"
-}
-
-const data2 = {
-     temperature: 15,
-     weatherState: WINDY,
-     humidity: 76,
-     wind: "20 m/s"
-}
-const city2 = "Bogotá"
-
-
-
+const location = "Buenos Aires,ar";
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 
 const WeatherLocation = () => {
-     const [city, setCity] = useState("Buenos Aires")
+  const data1 = {
+    temperature: 5,
+    weatherState: SUN,
+    humidity: 10,
+    wind: "10 m/s"
+  };
 
-     const handleUpdateClick = (props) => {
-          console.log("Updated!");
-          setCity(city2)                             
-      }
+  const city1 = "Bogota";
 
-     return(
-          <div className="weatherLocationCont">
-               <Location city={city}></Location>
-               <WeatherData data={data}></WeatherData>
-               <button className="buttonWeatherLocation" onClick={handleUpdateClick}>Update</button>
-          </div>
-     )
+  const [city, setCity] = useState(city1);
+  const [data, setData] = useState(data1);
+  const [isFirst, setIsFirst] = useState(true);
+
+async function handleUpdateClick() {
+    let nuevos= await getAPI(api_weather);
+    console.log("Updated!");
+    setCity(isFirst ? nuevos.city : city1);
+    setData(isFirst ? nuevos.new_weather : data1);
+
+    setIsFirst(!isFirst);
+  }
+
+   function getData(json) {
+     const {humidity, temp} = json.main;
+     const speed = json.wind.speed;
+     const weatherState = SUN;
+     const city = json.name
+     
+     let nuevosDatos = {
+     humidity: humidity,
+     temperature: parseInt((temp - 273), 10),
+     weatherState: weatherState,
+     wind:  `${speed} m/s`
+     };
+
+     let todos = {
+          new_weather: nuevosDatos,
+          city: city
+     }
+
+     return todos;
+  }
+
+function getAPI(url) {
+    return fetch(url)
+      .then(res => {           
+        return res.json();
+      })
+      .then(jsonResponse => {          
+        return getData(jsonResponse);
+      });
+  };
+
+  return (
+    <div className="weatherLocationCont">
+      <Location city={city}></Location>
+      <WeatherData data={data}></WeatherData>
+      <button className="buttonWeatherLocation" onClick={handleUpdateClick}>
+        Update
+      </button>
+    </div>
+  );
 };
 
 export default WeatherLocation;
+
+WeatherLocation.propTypes = {
+     data: PropTypes.shape({
+       temperature: PropTypes.number.isRequired,
+       weatherState: PropTypes.string.isRequired,
+       humidity: PropTypes.number.isRequired,
+       wind: PropTypes.string.isRequired
+     })
+   };
