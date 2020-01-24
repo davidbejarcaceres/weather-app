b54436074bf01e337f758a3d899c016e7b2e2bb7
David Béjar Cáceres <davidbejar2009@gmail.com>
2020-01-24 11:32:03 +0100

    material react eslint, lecture 58

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -5,37 +5,49 @@ import PropTypes from "prop-types";
 import { SUN, WINDY } from "../../consants/weather";
 import { useState, useEffect  } from "react";
 import api_weather from "../../consants/weather_api";
+import CircularProgress from "@material-ui/core/CircularProgress"
+
+
+
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345,
+  },
+});
 
 
 const WeatherLocation = () => {
-  const data1 = {
-    temperature: 5,
-    weatherState: SUN,
-    humidity: 10,
-    wind: "10 m/s"
-  };
+  const classes = useStyles();
 
-  // handleUpdateClick
-  const [city, setCity] = useState("loading..");
+  const [city, setCity] = useState("Buenos Aires");
   const [data, setData] = useState();
-  const [isFirst, setIsFirst] = useState(true);
-
+  
+  
   useEffect(() => {
     const fetchData = async () => {
       let nuevos= await getAPI(api_weather);
       setData(nuevos.new_weather);
       setCity(nuevos.city);
+      
     };
     
     fetchData();
-  }, []);
-
+  }, []); // Inside of the brackets input the variable as a listener to run the async function again
+    
 
 async function handleUpdateClick() {  
     let nuevos= await getAPI(api_weather);
     console.log("Updated!");
-    setData(nuevos.new_weather);  
-    setCity(nuevos.city);
+
   }
 
    function getData(json) {
@@ -69,15 +81,14 @@ function getAPI(url) {
       });
   };
 
+
   return (
     <div className="weatherLocationCont">
+      <Location city={city}></Location>
       {
-        (city) ? <Location city={city}></Location> : <Location city={"loading..."}></Location>
+        (data) ? <WeatherData data={data}></WeatherData> : <CircularProgress size={"3em"}></CircularProgress>
       }
-      {
-        (data) ? <WeatherData data={data}></WeatherData> : <span></span>
-      }
-
+      <br></br>
       <button className="buttonWeatherLocation" onClick={handleUpdateClick}>
         Update
       </button>
